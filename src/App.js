ec8f6499e1b444a21a5de1247188d9a4a67a4002
parnorma <parnorma@gmail.com>
2022-11-25 11:36:12 -0300

    varios

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+import Boton from "./components/Boton";
+import Contador from "./components/Contador";
+import freeCodeCampLogo from "./images/freecodecamp-logo.png";
+// para poder usar hooks (funciones para manejar estados en ls componentes
+import { useState } from "react";
+function App() {
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () => {
+    setNumClics(numClics + 1);
+  };
+
+  const reiniciarContador = () => {
+    setNumClics(0);
+  };
+
+  return (
+    <div className="App">
+      <div className="freecodecamp-logo-contenedor">
+        <img className="freecodecamp-logo" src={freeCodeCampLogo} alt="" />
+      </div>
+      <div className="contenedor-principal">
+        <Contador numClics={numClics} />
+        <Boton texto="Clic" esBotonDeClic={true} manejarClic={manejarClic} />
+        <Boton
+          texto="Reiniciar"
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
